5cd00248d00db00948a2f3b32ed6300dbf905e76
Burton Smith <burton@breakstuff.io>
2023-02-26 22:20:52 -0500

    add error if no cem

--- src/storybook-utils.ts
+++ src/storybook-utils.ts
@@ -1,6 +1,6 @@
-import { TemplateResult } from 'lit';
-import { ArgTypes } from './storybook';
-import { getStyleTemplate, getTemplate } from './html-templates.js';
+import { TemplateResult } from "lit";
+import { ArgTypes } from "./storybook";
+import { getStyleTemplate, getTemplate } from "./html-templates.js";
 import {
   getComponentByTagName,
   getCssParts,
@@ -9,8 +9,8 @@ import {
   getReactEvents,
   getReactProperties,
   getSlots,
-} from './cem-utilities.js';
-import { Declaration } from './cem-schema';
+} from "./cem-utilities.js";
+import { Declaration } from "./cem-schema";
 
 /**
  * Gets Storybook helpers for a given component
@@ -18,27 +18,31 @@ import { Declaration } from './cem-schema';
  * @returns An object containing the argTypes, reactArgTypes, events, styleTemplate, and template
  */
 export function getWcStorybookHelpers(tagName: string) {
-  /** 
-   * 
-   * uses the global window.__STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__ 
-   * variable created by the Storybook `setCustomElementsManifest` 
-   * method in the `preview.cjs` file 
-   * 
+  /**
+   *
+   * uses the global window.__STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__
+   * variable created by the Storybook `setCustomElementsManifest`
+   * method in the `preview.cjs` file
+   *
    */
-  const cem = (window as any).__STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__;  
+  const cem = (window as any).__STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__;
+  if(!cem) {
+    throw new Error("Custom Elements Manifest not found. Be sure to follow the pre-install steps in this guide:\nhttps://www.npmjs.com/package/wc-storybook-helpers#before-you-install");
+  }
   const component = getComponentByTagName(tagName, cem);
-  const eventNames = component?.events?.map(event => event.name) || [];
+  const eventNames = component?.events?.map((event) => event.name) || [];
 
   return {
     argTypes: getArgTypes(component),
     reactArgTypes: getReactProps(component),
     events: eventNames,
     styleTemplate: (args?: any) => getStyleTemplate(component, args),
-    template: (args?: any, slot?: TemplateResult) => getTemplate(component, args, slot),
+    template: (args?: any, slot?: TemplateResult) =>
+      getTemplate(component, args, slot),
   };
 }
 
-function getArgTypes(component?: Declaration): ArgTypes {  
+function getArgTypes(component?: Declaration): ArgTypes {
   const argTypes: ArgTypes = {
     ...getAttributesAndProperties(component),
     ...getCssProperties(component),
