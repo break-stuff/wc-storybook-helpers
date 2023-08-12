708405452716f036caedb843a2c4595c14d6590e
break-stuff <burton@breakstuff.io>
2023-08-12 16:44:22 -0400

    add upgraded demo

---
+++ demo/lit-app-v7/.storybook/preview.ts
@@ -0,0 +1,19 @@
+import type { Preview } from "@storybook/web-components";
+import { setCustomElementsManifest } from "@storybook/web-components";
+import customElements from "../custom-elements.json";
+
+setCustomElementsManifest(customElements);
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
